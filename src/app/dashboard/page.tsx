e8174f25c7ce054551fd5539e885646f320a3ea1
mohammad-khs <101863197+mohammad-khs@users.noreply.github.com>
2024-09-13 11:58:31 +0330

    authentication at the test phase

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/button";
+import { FC } from "react";
+
+interface Props {}
+
+const page: FC<Props> = ({}) => {
+  return <Button>hello</Button>;
+};
+
+export default page;
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import { Loader2 } from "lucide-react";
+import { ButtonHTMLAttributes, FunctionComponent } from "react";
+
+const buttonVariants = cva(
+  "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
+  {
+    variants: {
+      varinat: {
+        default: "bg-slate-900 text-white hover:bg-slate-800",
+        ghost: "bg-transparent hover:text-slate-900 hover:bg-slate-200",
+      },
+      size: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 px-2",
+        lg: "h-11 px-8",
+      },
+    },
+    defaultVariants: { varinat: "default", size: "default" },
+  }
+);
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+const Button: FunctionComponent<ButtonProps> = ({
+  className,
+  varinat,
+  children,
+  isLoading,
+  size,
+  ...props
+}) => {
+  return (
+    <button
+      className={cn(buttonVariants({ varinat, size, className }))}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? <Loader2 className="mr-2  w-4 animate-spin" /> : null}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
