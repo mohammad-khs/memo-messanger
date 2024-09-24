4160e446d6c53faa72f33946ca871f09f4abb1a5
mohammad-khs <101863197+mohammad-khs@users.noreply.github.com>
2024-09-24 21:28:43 +0330

    relocated loading of [chatId] component

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -7,6 +7,27 @@ import { getServerSession } from "next-auth";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+// The following generateMetadata functiion was written after the video and is purely optional
+export async function generateMetadata({
+  params,
+}: {
+  params: { chatId: string };
+}) {
+  const session = await getServerSession(authOptions);
+  if (!session) notFound();
+  const [userId1, userId2] = params.chatId.split("--");
+  const { user } = session;
+
+  const chatPartnerId = user.id === userId1 ? userId2 : userId1;
+  const chatPartnerRaw = (await fetchRedis(
+    "get",
+    `user:${chatPartnerId}`
+  )) as string;
+  const chatPartner = JSON.parse(chatPartnerRaw) as User;
+
+  return { title: `FriendZone | ${chatPartner.name} chat` };
+}
+
 interface PageProps {
   params: {
     chatId: string;
@@ -21,11 +42,12 @@ async function getChatMessages(chatId: string) {
       0,
       -1
     );
+
     const dbMessages = results.map((message) => JSON.parse(message) as Message);
 
-    const reverseDBMessages = dbMessages.reverse();
+    const reversedDbMessages = dbMessages.reverse();
 
-    const messages = messageArrayValidator.parse(reverseDBMessages);
+    const messages = messageArrayValidator.parse(reversedDbMessages);
 
     return messages;
   } catch (error) {
@@ -33,11 +55,9 @@ async function getChatMessages(chatId: string) {
   }
 }
 
-const Page = async ({ params }: PageProps) => {
-  await new Promise((reslove) => setTimeout(reslove, 10000));
+const page = async ({ params }: PageProps) => {
   const { chatId } = params;
   const session = await getServerSession(authOptions);
-
   if (!session) notFound();
 
   const { user } = session;
@@ -49,6 +69,7 @@ const Page = async ({ params }: PageProps) => {
   }
 
   const chatPartnerId = user.id === userId1 ? userId2 : userId1;
+  // new
 
   const chatPartnerRaw = (await fetchRedis(
     "get",
@@ -58,42 +79,43 @@ const Page = async ({ params }: PageProps) => {
   const initialMessages = await getChatMessages(chatId);
 
   return (
-    <>
-      <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]">
-        <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
-          <div className="relative flex items-center space-x-4">
-            <div className="relative ">
-              <div className="relative w-8 sm:w-12 h-8 sm:h-12">
-                <Image
-                  fill={true}
-                  referrerPolicy="no-referrer"
-                  src={chatPartner.image}
-                  alt={`${chatPartner.name} profile picture`}
-                  className="rounded-full"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col leading-tight">
-              <div className="text-xl flex items-center">
-                <span className="text-gray-700 mr-3 font-semibold ">
-                  {chatPartner.name}
-                </span>
-              </div>
-              <span className="text-sm text-gray-600">{chatPartner.email}</span>
+    <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]">
+      <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+        <div className="relative flex items-center space-x-4">
+          <div className="relative">
+            <div className="relative w-8 sm:w-12 h-8 sm:h-12">
+              <Image
+                fill
+                referrerPolicy="no-referrer"
+                src={chatPartner.image}
+                alt={`${chatPartner.name} profile picture`}
+                className="rounded-full"
+              />
             </div>
           </div>
+
+          <div className="flex flex-col leading-tight">
+            <div className="text-xl flex items-center">
+              <span className="text-gray-700 mr-3 font-semibold">
+                {chatPartner.name}
+              </span>
+            </div>
+
+            <span className="text-sm text-gray-600">{chatPartner.email}</span>
+          </div>
         </div>
-        <Messages
-          chatId={chatId}
-          chatPartner={chatPartner}
-          sessionImg={session.user.image}
-          initialMessages={initialMessages}
-          sessionId={session.user.id}
-        />
-        <ChatInput chatId={chatId} chatPartner={chatPartner} />
       </div>
-    </>
+
+      <Messages
+        chatId={chatId}
+        chatPartner={chatPartner}
+        sessionImg={session.user.image}
+        sessionId={session.user.id}
+        initialMessages={initialMessages}
+      />
+      <ChatInput chatId={chatId} chatPartner={chatPartner} />
+    </div>
   );
 };
 
-export default Page;
+export default page;
